7a4db1fc5051fab832a8e6158046eaf9d04e9a48
invernoe <40373518+invernoe@users.noreply.github.com>
2024-07-15 21:55:50 +0200

    move program file

---
+++ Assignment08/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Assignment8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
